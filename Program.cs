c26a158f08c29c206d552bcdde9f71261ae50378
GodOfAllBeings <s174871@student.dtu.dk>
2023-02-24 12:26:18 +0100

    Just removed unused code

--- Program.cs
+++ Program.cs
@@ -31,26 +31,26 @@ namespace Repository
 
             app.UseAuthorization();
 
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
+            //var summaries = new[]
+            //{
+            //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            //};
 
             app.MapGet("/", () => "Hello World!");
 
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateTime.Now.AddDays(index),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
+            //app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            //{
+            //    var forecast = Enumerable.Range(1, 5).Select(index =>
+            //        new WeatherForecast
+            //        {
+            //            Date = DateTime.Now.AddDays(index),
+            //            TemperatureC = Random.Shared.Next(-20, 55),
+            //            Summary = summaries[Random.Shared.Next(summaries.Length)]
+            //        })
+            //        .ToArray();
+            //    return forecast;
+            //})
+            //.WithName("GetWeatherForecast");
 
             _ = new Endpoints.Endpoints(app);
             //_ = new Requests.Requests(app);
