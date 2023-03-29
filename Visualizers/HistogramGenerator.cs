a8f13ec0dc797640d194413e3a5342315a7db5a7
Squidman95 <squidmanse@gmail.com>
2023-03-29 13:26:32 +0200

    Histogram cleanup

--- Visualizers/HistogramGenerator.cs
+++ Visualizers/HistogramGenerator.cs
@@ -13,12 +13,11 @@ namespace Repository.Visualizers
         static readonly string pathToJson = Path.Combine(pathToResources, "JSON");
         public static IResult GetHistogram(string resourceId, string appUrl)
         {
-            Dictionary<string, int> histogramDict = new Dictionary<string, int>();
-            // TODO: Check if a histogram already exists for this file
             Console.WriteLine("Creating histogram for requested object: " + resourceId);
             MetadataObject? metadataObject = DBManager.GetMetadataObjectById(resourceId);
             if (metadataObject == null || metadataObject.ResourceInfo?.FileExtension == null) return Results.BadRequest("Invalid resource ID. No reference to resource could be found.");
-            string pathToFileExtension = Path.Combine(pathToResources, metadataObject.ResourceInfo.FileExtension.ToUpper()); // TODO: Add null check or try/catch
+            
+            string pathToFileExtension = Path.Combine(pathToResources, metadataObject.ResourceInfo.FileExtension.ToUpper());
             string pathToFile = Path.Combine(pathToFileExtension, resourceId + "." + metadataObject.ResourceInfo.FileExtension);
             if (!File.Exists(pathToFile) || metadataObject.ResourceInfo?.ResourceType != "EventLog")
             {
@@ -26,68 +25,25 @@ namespace Repository.Visualizers
                 return Results.BadRequest(badResponse);
             }
 
-            //List<Child> children = metadataObject.GenerationTree.Children;
-            List<string> childrenIds = metadataObject.GenerationTree?.Children?.Select(child => child.ResourceId).ToList();
+            List<string>? childrenIds = metadataObject.GenerationTree?.Children?.Select(child => child.ResourceId).ToList();
             foreach (var childId in childrenIds ?? Enumerable.Empty<string>())
             {
                 Console.WriteLine("Child id: " + childId);
                 var childMetadata = DBManager.GetMetadataObjectById(childId);
                 if (childMetadata != null && childMetadata.ResourceInfo.ResourceType == "Histogram") return ResourceRetriever.GetResourceById(childId);
             }
-            //var hist = children.Select(child => child.r filters.Any(filter => resource.ResourceInfo.ResourceType.Equals(filter, StringComparison.OrdinalIgnoreCase)));
 
-            XmlDocument doc = new XmlDocument();
-            doc.Load(pathToFile);
-            foreach (XmlNode traceNode in doc.DocumentElement.ChildNodes)
-            {
-                if (traceNode.Name == "trace")
-                {
-                    //Console.WriteLine($"\n\nNew Trace:");
-                    foreach (XmlNode eventNode in traceNode.ChildNodes)
-                    {
-                        if (eventNode.Name == "event")
-                        {
-                            foreach (XmlNode eventAttribute in eventNode.ChildNodes)
-                            {
-                                string eventKey = eventAttribute.Attributes["key"].Value;
-                                if(eventKey == "concept:name")
-                                {
-                                    string eventValue = eventAttribute.Attributes["value"].Value;
-                                    //Console.WriteLine("EventKey: " + eventKey);
-                                    //Console.WriteLine("Attribute value: " + eventValue);
-                                    if (histogramDict.ContainsKey(eventValue))
-                                    {
-                                        histogramDict[eventValue] += 1;
-                                    }
-                                    else
-                                    {
-                                        histogramDict[eventValue] = 1;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            //[
-            //    ["event1", 1000],
-            //    ["event2", 1170],
-            //    ["event3", 660],
-            //    ["event4", 1030],
-            //]
-            List<List<dynamic>> histogramList = new();
-            foreach (var eventDict in histogramDict)
-            {
-                List<dynamic> tmpList = new()
-                {
-                    eventDict.Key,
-                    eventDict.Value,
-                };
-                histogramList.Add(tmpList);
-            }
-            var jsonList = JsonConvert.SerializeObject(histogramList, Newtonsoft.Json.Formatting.Indented);
-            Console.WriteLine(jsonList);
+            var histogramDict = GenerateHistogramDict(pathToFile);
+            string jsonList = ConvertToJsonList(histogramDict);
+            string pathToSave = AddHistogramToMetadata(resourceId, appUrl, metadataObject);
 
+            File.WriteAllText(pathToSave, jsonList);
+            return Results.Text(jsonList, contentType: "application/json");
+            //return Results.File(pathToSave, GUID); // If we would want to return the file instead?
+        }
+
+        private static string AddHistogramToMetadata(string resourceId, string appUrl, MetadataObject? metadataObject)
+        {
             string resourceLabel = $"Histogram from log: {metadataObject.ResourceInfo.ResourceLabel}";
             string GUID = Guid.NewGuid().ToString();
             string host = $"{appUrl}/resources/";
@@ -106,9 +62,65 @@ namespace Repository.Visualizers
             //DBManager.AddToMetadata(resourceLabel, resourceType: "Histogram", GUID, host, generatedFrom: generatedFromString, parents: parentsString, description, fileExtension: "json");
             DBManager.AddToMetadata(resourceLabel, resourceType: "Histogram", GUID, host, generatedFrom: generatedFrom, parents: parents, description, fileExtension: "json");
             string pathToSave = Path.Combine(pathToJson, $"{GUID}.json");
-            File.WriteAllText(pathToSave, jsonList);
-            return Results.Text(jsonList, contentType: "application/json");
-            //return Results.File(pathToSave, GUID);
+            return pathToSave;
+        }
+
+        // Convert dictionary to list in format that Frontend takes
+        private static string ConvertToJsonList(Dictionary<string, int> histogramDict)
+        {
+            List<List<dynamic>> histogramList = new();
+            foreach (var eventDict in histogramDict)
+            {
+                List<dynamic> tmpList = new()
+                {
+                    eventDict.Key,
+                    eventDict.Value,
+                };
+                histogramList.Add(tmpList);
+            }
+            var jsonList = JsonConvert.SerializeObject(histogramList, Newtonsoft.Json.Formatting.Indented);
+            return jsonList;
+        }
+
+        // Count number of events and save in histogram dictionary
+        private static Dictionary<string, int> GenerateHistogramDict(string pathToFile)
+        {
+            Dictionary<string, int> histogramDict = new Dictionary<string, int>();
+            XmlDocument doc = new XmlDocument();
+            doc.Load(pathToFile);
+            foreach (XmlNode traceNode in doc.DocumentElement.ChildNodes)
+            {
+                if (traceNode.Name == "trace")
+                {
+                    //Console.WriteLine($"\n\nNew Trace:");
+                    foreach (XmlNode eventNode in traceNode.ChildNodes)
+                    {
+                        if (eventNode.Name == "event")
+                        {
+                            foreach (XmlNode eventAttribute in eventNode.ChildNodes)
+                            {
+                                string eventKey = eventAttribute.Attributes["key"].Value;
+                                if (eventKey == "concept:name")
+                                {
+                                    string eventValue = eventAttribute.Attributes["value"].Value;
+                                    //Console.WriteLine("EventKey: " + eventKey);
+                                    //Console.WriteLine("Attribute value: " + eventValue);
+                                    if (histogramDict.ContainsKey(eventValue))
+                                    {
+                                        histogramDict[eventValue] += 1;
+                                    }
+                                    else
+                                    {
+                                        histogramDict[eventValue] = 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return histogramDict;
         }
     }
 }
