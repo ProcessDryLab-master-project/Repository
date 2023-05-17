d89a6ce23b6f743631218acd2e5b90828c3cc650
Squidman95 <squidmanse@gmail.com>
2023-05-17 13:43:19 +0200

    Retry write file

--- App/Database/FileDb.cs
+++ App/Database/FileDb.cs
@@ -10,6 +10,8 @@ namespace Repository.App.Database
     {
         static readonly ReaderWriterLock fileLock = new ReaderWriterLock();
         static readonly int milliSecTimeout = 60000; // Timeout after 60 sec
+        private const int NumberOfRetries = 3;
+        private const int DelayOnRetry = 1000;
         public void CopyStream(Stream stream, string downloadPath)
         {
             using (var fileStream = new FileStream(downloadPath, FileMode.Create, FileAccess.Write))
@@ -40,41 +42,86 @@ namespace Repository.App.Database
                 return Results.BadRequest("Repository GetFileById error: " + e);
             }
         }
+
         public async Task<IResult> WriteFile(MetadataObject metadataObject, IFormFile file)
         {
-            try
+            IOException errorBackup = new IOException("IOException");
+            for (int i = 1; i <= NumberOfRetries; ++i)
             {
-                fileLock.AcquireWriterLock(milliSecTimeout);
-                Console.WriteLine("File lock set");
-                string path = DbHelper.GetFileSavePath(metadataObject);
-                Console.WriteLine($"Attempting to write file for id {metadataObject.ResourceId} to path {path}");
-
-                //using var stream = File.OpenWrite(path);
-                //await file.CopyToAsync(stream);
-
-                // If we read IFormFile into byte[] first:
-                //var content = new MemoryStream(file);
-                //CopyStream(content, path);
-
-                // If we use IFormFile "file" instead:
-                await using (var fileStream = File.Open(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+                try
                 {
-                    fileStream.SetLength(0); // truncate the file
-                    file.CopyTo(fileStream); // write to the file
+                    fileLock.AcquireWriterLock(milliSecTimeout);
+                    Console.WriteLine("File lock set");
+                    string path = DbHelper.GetFileSavePath(metadataObject);
+                    Console.WriteLine($"Attempting to write file for id {metadataObject.ResourceId} to path {path}");
+
+                    using var stream = File.OpenWrite(path);
+                    await file.CopyToAsync(stream);
+
+
+                    // If we read IFormFile into byte[] first:
+                    //var content = new MemoryStream(file);
+                    //CopyStream(content, path);
+
+                    // This can force the deserializing error.
+                    //await using (var fileStream = File.Open(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+                    //{
+                    //    fileStream.SetLength(0); // truncate the file
+                    //    file.CopyTo(fileStream); // write to the file
+                    //}
+
+                    fileLock.ReleaseWriterLock();
+                    Console.WriteLine("File lock released");
+
+                    Console.WriteLine("Done, returning ok");
+                    return Results.Ok(metadataObject.ResourceId);
+                    //break; // When done we can break loop
+                }
+                catch (IOException e) when (i <= NumberOfRetries)
+                {
+                    // You may check error code to filter some exceptions, not every error can be recovered.
+                    Console.WriteLine($"Failed to save file on attempt {i}, with exception {e}");
+                    errorBackup = e;
+                    Thread.Sleep(DelayOnRetry);
                 }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error while saving incoming file: " + e);
-                return Results.BadRequest("Repository WriteFile error:" + e);
-            }
-            finally
-            {
-                fileLock.ReleaseWriterLock();
-                Console.WriteLine("File lock released");
-            }
-            Console.WriteLine("Done, returning ok");
-            return Results.Ok(metadataObject.ResourceId);
+            return Results.BadRequest(errorBackup);
         }
+        //public async Task<IResult> WriteFile(MetadataObject metadataObject, IFormFile file)
+        //{
+        //    try
+        //    {
+        //        fileLock.AcquireWriterLock(milliSecTimeout);
+        //        Console.WriteLine("File lock set");
+        //        string path = DbHelper.GetFileSavePath(metadataObject);
+        //        Console.WriteLine($"Attempting to write file for id {metadataObject.ResourceId} to path {path}");
+
+        //        using var stream = File.OpenWrite(path);
+        //        await file.CopyToAsync(stream);
+
+        //        // If we read IFormFile into byte[] first:
+        //        //var content = new MemoryStream(file);
+        //        //CopyStream(content, path);
+
+        //        // This can force the deserializing error.
+        //        //await using (var fileStream = File.Open(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+        //        //{
+        //        //    fileStream.SetLength(0); // truncate the file
+        //        //    file.CopyTo(fileStream); // write to the file
+        //        //}
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("Error while saving incoming file: " + e);
+        //        return Results.BadRequest("Repository WriteFile error:" + e);
+        //    }
+        //    finally
+        //    {
+        //        fileLock.ReleaseWriterLock();
+        //        Console.WriteLine("File lock released");
+        //    }
+        //    Console.WriteLine("Done, returning ok");
+        //    return Results.Ok(metadataObject.ResourceId);
+        //}
     }
 }
